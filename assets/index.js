ba40d09d18e4c1144654c07b9ba7b94d30809658
Malak Markus <malak.markus@outlook.com>
2022-07-13 19:28:01 -0400

    add query selectors and updating the html and css files

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,10 @@
 var cities = [];
-var apiKey = "fe3637fd42a277b6822b220a75b29af3"
+var apiKey = "fe3637fd42a277b6822b220a75b29af3";
+
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#search-history");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var savedSearchButtonEl = document.querySelector("#saved-search-buttons");
